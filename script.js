a1014da6bd1ad0731764bef8b276a65ba07120c2
Kedzie <kedziestark@gmail.com>
2022-07-05 15:27:42 -0400

    Refactor/clean up js file

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
+// Get page elements, initialize grid variables
 const container = document.querySelector('.container');
 const clearGridBtn = document.querySelector('.clearGridBtn');
 const resizeGridBtn = document.querySelector('.resizeGridBtn');
+let squares;
+clearGridBtn.addEventListener('click', clearGrid);
+resizeGridBtn.addEventListener('click', gridResize);
 
 
 // Create square grid
-
 for (let i = 0; i < 256; i++) {
     const square = document.createElement('div');
     square.classList.add("square","ini");
     container.append(square);
 }
 
-let squares = document.querySelectorAll('.square');
+// Create utility functions
+function mouseOver() {
+    squares = document.querySelectorAll('.square');
+    squares.forEach((square) => {
+        square.addEventListener('mouseover', () => {
+            square.classList.add('active');
+        });
+    })
+}
 
-squares.forEach((square) => {
-    square.addEventListener('mouseover', () => {
-        square.classList.add('active');
-    });
-});
-
-// Clear grid
 function clearGrid() {
     squares.forEach((square) => {
         square.classList.remove('active');
@@ -27,25 +31,18 @@ function clearGrid() {
 }
 
 function gridResize() {
-    container.replaceChildren();
     let gridSize = prompt("Enter a grid size","16");
-    let newSquareSize = (100 / gridSize);
+    container.replaceChildren();
     for (let i = 0; i < Math.pow(gridSize, 2); i++) {
         const square = document.createElement("div");
-        square.classList.add("square","testClass");
-        square.style.setProperty("width", `calc(${newSquareSize}%)`);
+        square.classList.add("square");
+        square.style.setProperty("width", `calc(${100 / gridSize}%)`);
         container.append(square);
         //addHover();
     }
-    squares = document.querySelectorAll('.testClass');
-    squares.forEach((square) => {
-        square.addEventListener('mouseover', () => {
-            square.classList.add('active');
-        });
-    });
+    mouseOver();
 }
 
-clearGridBtn.addEventListener('click', clearGrid);
-resizeGridBtn.addEventListener('click', gridResize);
+// 
 
-
+mouseOver();
