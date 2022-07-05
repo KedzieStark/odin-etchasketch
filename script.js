3cba3a09cab7eda33488789d682b9e76ccb637b1
Kedzie <kedziestark@gmail.com>
2022-07-05 12:58:17 -0400

    Refactor resize function

--- script.js
+++ script.js
@@ -2,23 +2,22 @@ const container = document.querySelector('.container');
 const clearGridBtn = document.querySelector('.clearGridBtn');
 const resizeGridBtn = document.querySelector('.resizeGridBtn');
 
+
 // Create square grid
 
 for (let i = 0; i < 256; i++) {
     const square = document.createElement('div');
-    square.className = "square";
+    square.classList.add("square","ini");
     container.append(square);
 }
 
-// Hover effect
-
-const squares = document.querySelectorAll('.square');
+let squares = document.querySelectorAll('.square');
 
 squares.forEach((square) => {
     square.addEventListener('mouseover', () => {
         square.classList.add('active');
-    })
-})
+    });
+});
 
 // Clear grid
 function clearGrid() {
@@ -28,8 +27,25 @@ function clearGrid() {
 }
 
 function gridResize() {
-    let gridSize = prompt("Enter a grid size","16");
     container.replaceChildren();
+    let gridSize = prompt("Enter a grid size","16");
+    let newSquareSize = (100 / gridSize);
+    for (let i = 0; i < Math.pow(gridSize, 2); i++) {
+        const square = document.createElement("div");
+        square.classList.add("square","testClass");
+        square.style.setProperty("width", `calc(${newSquareSize}%)`);
+        container.append(square);
+        //addHover();
+    }
+    squares = document.querySelectorAll('.testClass');
+    squares.forEach((square) => {
+        square.addEventListener('mouseover', () => {
+            square.classList.add('active');
+        });
+    });
 }
 
 clearGridBtn.addEventListener('click', clearGrid);
+resizeGridBtn.addEventListener('click', gridResize);
+
+
